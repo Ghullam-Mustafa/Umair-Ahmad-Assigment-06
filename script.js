1ab8a38e230898b57bd2a30478e49b573b9d201f
Ghullam Mustafa <gmustafawaince180@gmail.com>
2023-05-26 23:10:33 +0500

    Rendom

--- script.js
+++ script.js
@@ -1,34 +1,45 @@
-// // ---------------------value------------------------------
-//    let inputValue = document.getElementById('data').value
-// // ----------------------input erase ------------------------
-// document.getElementById('erase').onclick = function () {
-//     document.getElementById('data').value = "";
-// }
+// ---------------------value------------------------------
+   let inputValue = document.getElementById('data').value
 
-// // -----------------------output--------------------
-// document.getElementById('Clean').onclick = function () {
-//     let outputBoxHtml =  document.getElementById('output').innerHTML ;
+// -------------------------------------------------------------
+
+let rightNowDate = new Date();
+console.log(rightNowDate.getDay());
+// rightNowDate = rightNowDate.toDateString()
+document.getElementById('current').innerHTML = rightNowDate.getDay();
+// -----------------------------------------------------------------------------------
+
+    let daysName = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
+    console.log(daysName[rightNowDate.getDay()]);
+// ----------------------input erase ------------------------
+document.getElementById('erase').onclick = function () {
+    document.getElementById('data').value = "";
+}
+
+// -----------------------output--------------------
+document.getElementById('Clean').onclick = function () {
+    let outputBoxHtml =  document.getElementById('output').innerHTML ;
  
-//     if (!outputBoxHtml.length) {
-//      Toastify({
-//          text: "It's already empty",
-//          className: "info",
-//          newWindow: true,
-//          close: true,
-//          style: {
-//            background: "linear-gradient(to right, #00000, #96c93d)",
-//          }
-//        }).showToast();
-//     }else{
-//      clearOutput();
-//      Toastify({
-//          text: "output result has been cleard",
-//          className: "info",
-//          newWindow: true,
-//          close: true,
-//          style: {
-//            background: "linear-gradient(to right, #0009b, #96c93d)",
-//          }
-//        }).showToast();
-//     }
-//  }
+    if (!outputBoxHtml.length) {
+     Toastify({
+         text: "It's already empty",
+         className: "info",
+         newWindow: true,
+         close: true,
+         style: {
+           background: "linear-gradient(to right, #00000, #96c93d)",
+         }
+       }).showToast();
+    }else{
+     clearOutput();
+     Toastify({
+         text: "output result has been cleard",
+         className: "info",
+         newWindow: true,
+         close: true,
+         style: {
+           background: "linear-gradient(to right, #0009b, #96c93d)",
+         }
+       }).showToast();
+    }
+ }
