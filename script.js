98d5c332b598af4da21e80810cb354ab49761010
Ghullam Mustafa <gmustafawaince180@gmail.com>
2023-06-03 22:43:20 +0500

    asdffff

--- script.js
+++ script.js
@@ -215,6 +215,14 @@ document.getElementById('Clean').onclick = function () {
     document.getElementById('output').innerHTML = output
  }
 
+
+
+
+
+
+
+
+ 
 //  --------------------------showOutput--------------------------
 // function input() {
 //    return someText = document.getElementById('data').value ;
@@ -235,20 +243,20 @@ document.getElementById('Clean').onclick = function () {
 // console.log(gggg);
 
 // // ---------------------
-// function getTimeOfDay() {
-//     var currentTime = new Date();
-//     var hours = currentTime.getHours();
+function getTimeOfDay() {
+    var currentTime = new Date();
+    var hours = currentTime.getHours();
   
-//     if (hours >= 1 && hours <= 11) {
-//       return "Good morning";
-//     } else if (hours >= 11 && hours <= 18) {
-//       return "Good afternoon";
-//     } else {
-//       return "Good night";
-//     }
-//   }
+    if (hours >= 1 && hours <= 11) {
+      return "Good morning";
+    } else if (hours >= 11 && hours <= 18) {
+      return "Good afternoon";
+    } else {
+      return "Good night";
+    }
+  }
   
-//   // Usage
-//   var greeting = getTimeOfDay();
-//   console.log(greeting);
+  // Usage
+  var greeting = getTimeOfDay();
+  console.log(greeting);
   
